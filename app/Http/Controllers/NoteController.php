9275c9e0fc5033b818181d02d723f4d849fc5738
unknown <you@example.com>
2024-04-10 12:58:09 +0300

    note index path added; route->controller->view

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -12,7 +12,7 @@ class NoteController extends Controller
      */
     public function index()
     {
-        //
+        return view('note.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,4 +10,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'welcome']);
 Route::get('/about', [WelcomeController::class, 'about']);
-// uztaisam html lapu about kurā ir tikai viens h1 elements
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
