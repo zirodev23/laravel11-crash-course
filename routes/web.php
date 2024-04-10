2bfa8b152eb1196d3241e1d1d3f5b186569d2cd2
unknown <you@example.com>
2024-04-10 13:12:55 +0300

    Note resource routes added

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@ Route::get('/', [WelcomeController::class, 'welcome']);
 Route::get('/about', [WelcomeController::class, 'about']);
 
 Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+Route::put('/note', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+
+// Route::resource('note', NoteController::class);
